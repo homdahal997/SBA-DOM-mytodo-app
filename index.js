84e51c972aa81fb3b273848cf8e49d4bc920c28e
Hom Dahal <homdahal997@gmail.com>
2024-05-03 15:59:22 -0400

    Function to handle checkbox to send checked active todos to Completed todo Implemented.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewTodos(){
         // - a span that displays the todo text
         data += `
         <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
-            <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." onclick="" />
+            <input class="form-check-input me-2" type="checkbox" value="" aria-label="..." onclick="completedTodo(${i})" />
             <span id="todo${i}">${todos[i]}</span>
         </li>
         `;
@@ -34,4 +34,30 @@ function viewTodos(){
 
     // Find the HTML element with the id "mytodo" and set its innerHTML to the data string
     document.getElementById("mytodo").innerHTML = data;
+}
+
+// Function for completed todos
+function completedTodo(i){
+    // Get todo element using todo name with corresponding index
+    let todoEl = document.getElementById(`todo${i}`);
+    // Clone the parent element of the todo element
+    let clonedNode = todoEl.parentElement.cloneNode(true);
+
+    // Get the checkbox from the cloned node
+    let checkbox = clonedNode.querySelector('input[type="checkbox"]');
+
+    // Remove the checkbox from the cloned node
+    clonedNode.removeChild(checkbox);
+
+    // Append the cloned node to the element with the id "completedTodo"
+    document.getElementById("completedTodo").appendChild(clonedNode);
+
+    // Remove the parent element of the original todo element
+    todoEl.parentElement.remove();
+
+    // Remove the todo at the given index from the todos array
+    todos.splice(i, 1);
+
+    // Update the todos
+    viewTodos();
 }
